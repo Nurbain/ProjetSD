7e95004f9e25e81ff7394b84e37430bc0439f15f
Nurbain <nathan.urbain@etu.unistra.fr>
2017-04-05 22:36:45 +0200

    Rename Joueur parce que eclipse rale

---
+++ Joueur.java
@@ -0,0 +1,11 @@
+import java.rmi.RemoteException;
+
+public class Joueur extends Client {
+
+	public Joueur(String name) throws RemoteException {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
