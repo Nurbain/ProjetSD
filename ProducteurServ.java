46d1177217ab67285dd2b991174798351146e0c2
nurbain <nathan.urbain@etu.unistra.fr>
2017-05-01 20:47:07 +0200

    Fichier commenté

--- ProducteurServ.java
+++ ProducteurServ.java
@@ -1,10 +1,13 @@
 import java.net.* ;
 import java.rmi.* ;
 
+/**@author WENDLING Quentin URBAIN Nathan*/
+
 public class ProducteurServ
 {
 	public static void main(String [] args)
 	{
+		//Verifie que toute les donnees sont rentre
 		if (args.length != 7)
 		{
 			System.out.println("Usage : java ProducteurServ <port du serveur de noms> <Nom Producteur> <Nom ressources> <nbr de ressource initiale> <ratio de prod> <Nb de ressources pouvant etre donnee> <y si tour par tour n sinon>") ;
@@ -12,9 +15,8 @@ public class ProducteurServ
 		}
 		try
 		{
+			//Cree le producteur
   			Producteur objLocal = new Producteur(args[1],"localhost",args[0],args[1],args[2],Integer.parseInt(args[3]),Float.valueOf(args[4]),Integer.parseInt(args[5]),(args[6].equals("y"))?true:false) ;
-			//Thread t=new Thread(objLocal);
-			//t.start();
 			Naming.rebind( "rmi://localhost:" + args[0] + "/" + args[1] ,objLocal);
 			System.out.println("Serveur pret");
 		}
--- Ressources.java
+++ Ressources.java
@@ -1,9 +1,15 @@
 import java.io.Serializable;
 
+/**@author WENDLING Quentin URBAIN Nathan*/
+
 public class Ressources implements Serializable{
 
   static final long serialVersionUID = 42;
-  private final String name;
+  
+	//Nom de la ressource 
+	private final String name;
+
+	//Nombre d'exemplaire de la ressource
   private int exemplaires;
 
   public Ressources(String name, int initnbr)
@@ -20,6 +26,7 @@ public class Ressources implements Serializable{
     return this.exemplaires;
   }
 
+	//Soustrait au nombre d'exemplaire le nombre donne arguement
   public synchronized Boolean takeRessources(int nb){
 		if(nb > exemplaires){
 		  return false;
@@ -28,10 +35,12 @@ public class Ressources implements Serializable{
 		return true;
   }
 
+	//Ajoute au nombre d'exemplaire le nombr donne en arguement
   public synchronized void addRessources(int nb){
     exemplaires += nb;
   }
 
+	//Test si une ressource passe en commentaire est eguale a l'actuel
   public boolean equals(Ressources r2){
 	  return r2.name.equals(this.name);
   }
