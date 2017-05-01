1cc6cf726dec1a0fc6f9bffb52e1c966d61a8220
nurbain <nathan.urbain@etu.unistra.fr>
2017-05-01 20:41:51 +0200

    fichier commenté

--- LogEntries.java
+++ LogEntries.java
@@ -1,8 +1,15 @@
 import java.io.Serializable;
 
+/**@author WENDLING Quentin URBAIN Nathan*/
+
+//Classe d'entree de log 
 public class LogEntries implements Serializable{
   static final long serialVersionUID = 42;
-  public long time;
+  
+	//Temps de l'action 
+	public long time;
+
+	//Action faite
   public String action;
 
   public LogEntries(long time,String action){
--- ObservateurServ.java
+++ ObservateurServ.java
@@ -1,10 +1,13 @@
 import java.net.* ;
 import java.rmi.* ;
 
+/**@author WENDLING Quentin URBAIN Nathan*/
+
 public class ObservateurServ
 {
 	public static void main(String [] args)
 	{
+		//Verifie que toute les donnees ont ete donnees 
 		if (args.length != 4)
 		{
 			System.out.println("Usage : java ObservateurServ <port du serveur de noms> <Nom Observateur> <y si tour par tour n sinon> <Nom du fichier Log>") ;
@@ -12,9 +15,8 @@ public class ObservateurServ
 		}
 		try
 		{
+			//Cree l'observateur
 			Observateur objLocal = new Observateur (args[1],"localhost",args[0],args[1],Fin.Brute,(args[2].equals("y"))?true:false , args[3]) ;
-			//Thread t=new Thread(objLocal);
-			//t.start();
 			Naming.rebind( "rmi://localhost:" + args[0] + "/" + args[1] ,objLocal);
 			System.out.println("Serveur pret") ;
 		}
